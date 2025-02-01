0714882e5dee6c795f70b0c585c1b039a2e463ce
Miki <bada.bing.bing@live.com>
2025-02-01 14:20:49 +0100

    ✨ Load ENV variables

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { ConfigLoader } from "./utils/configLoader";
 import { WorkTask } from "./tasks/workTask";
 import { TaskConfig } from "./types";
 import packageJson from "../package.json";
+import { env } from "./utils/envConfig";
 const program = new Command();
 
 program
@@ -20,11 +21,12 @@ program
   )
   .action(async (issueId: string, options: { config: string }) => {
     try {
-      // Load and validate configuration
+      // Load and validate configuration and env vars
       // TODO not sure if I want to use class to load and validate config
       const configLoader = new ConfigLoader(path.resolve(options.config));
       const config: TaskConfig = configLoader.load();
       configLoader.validate();
+      env.validate();
 
       // Execute tasks based on type
       switch (config.type) {
